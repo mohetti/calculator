1d89f27e4d3b1a27796a79f329551b6fa62c38d6
Mohetti <moritz.hettich@gmx.de>
2020-12-11 13:54:25 +0300

    all is working except decimal button. Maybe rewrite code

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let operator;
 let result;
 let resultWindow = document.getElementById("result");
 let ansWindow = document.getElementById("ans");
+let firstInput = [];
+let secondInput = [];
 
 // add function
 let add = function add(args) {
@@ -33,59 +35,103 @@ let div = function div(args) {
 };
 
 // transfers the numbers as html to the second screen
-let numberTransfer = function numberTransfer(htmlValue) {
+let numberFunction = function numberFunction(htmlValue) {
     if (result !== "") {
         result = "";
         return resultWindow.innerText = htmlValue;
     } else if(result === "") {
-        result = "";
         return resultWindow.innerText += htmlValue;
     }
-    
-    //return resultWindow.innerText += htmlValue;
 };
 
 
 
 // transfers the operator as html to the second screen and store operater in global variable
-let operatorTransfer = function operatorTransfer(htmlValue) {
+let operatorFunction = function operatorFunction(htmlValue) {
+    let containerOps = Array.from(resultWindow.innerText);
 
     let multipleOps = function multiplOps(input) {
-        if (input === "+" || input === "-" || input === "*" || input === "/") {
-        return true; 
-    } else {
-        return false;
-    };
+        if (input === "+" || input === "*" || input === "/" || 
+            String(resultWindow.innerText.slice(-1)) === "-" || resultWindow.innerText.indexOf("-", 1) > 0) {
+            return true; 
+        } else if (input === "-") {
+            if (String(resultWindow.innerText.slice(-1)) === "-") {
+            return true;
+        } else {
+            return false;
+        };
+        };
     };
 
-    let containerOps = Array.from(resultWindow.innerText);
-     /* im if statement kann das mit der ersten minus rein - dran denken dass result auf "" gesetzt wird
-     bei multipleOps muss - raus und in eine extra Funktion mit || rein
-     minus Funktion checkt, ob nur die drei minusse drin sind: 1. - muss zahl folgen, 2. zahl oder -, 3. zahl
-     operandArray muss anders gesplittet werden im Fall von -. Nach dem ersten -*/
-    if (resultWindow.innerText === "") {
-        return;
-    } else if (containerOps.some(multipleOps) === true) {
-        return;
-    } else {
-        operator = htmlValue;
+    let decimalCheck = function decimalCheck() {
+        return resultWindow.innerText.slice(-1) === "." ? true :  false;
+    }
+
+
+    if (resultWindow.innerText === "" && htmlValue === "-") {
+        operator = "";
         result = "";
-        resultWindow.innerText += htmlValue;
-    };
+        return resultWindow.innerText = htmlValue;
+    } else if (resultWindow.innerText !== "") {
+            switch (htmlValue) {
+                case "+":
+                    if (containerOps.some(multipleOps) === true || decimalCheck() === true) {
+                        return;
+                    } else {
+                        operator = htmlValue;
+                        result = "";
+                        resultWindow.innerText += htmlValue;
+                    }
+                    break;
+
+                case "*":
+                    if (containerOps.some(multipleOps) === true || decimalCheck() === true) {
+                        return;
+                    } else {
+                        operator = htmlValue;
+                        result = "";
+                        resultWindow.innerText += htmlValue;
+                    }
+                    break;
+
+                case "/":
+                    if (containerOps.some(multipleOps) === true || decimalCheck() === true) {
+                        return;
+                    } else {
+                        operator = htmlValue;
+                        result = "";
+                        resultWindow.innerText += htmlValue;
+                    }
+                    break;
+
+                case "-":
+                    if (result === "") {
+                    if (containerOps.some(multipleOps) === true || decimalCheck() === true) {
+                       return;
+                    } else if (resultWindow.innerText.indexOf("-", 1) === -1) {
+                        operator = htmlValue;
+                        result = "";
+                        resultWindow.innerText += htmlValue;
+                    } else if (resultWindow.innerText.indexOf("-", 1) > 0) {
+                        return;
+                    }
+                    } else if (result !== "") {
+                        operator = htmlValue;
+                        result = "";
+                        resultWindow.innerText += htmlValue;
+                    }
+                }
+
+            
+        }
 };
 
 // invoke equals-function
 let equalResult = function equalResult() {
-   
     let operandArray = resultWindow.innerText.split(operator);
-    if (operandArray.length < 2 || operandArray[1] === "") {
-        return;
-    } else {
+    if (operandArray.length >= 2 && operandArray[1] !== "") {
         ansWindow.innerText = resultWindow.innerText + " =";
         switch (operator) {
-                case "-":
-                    result = sub(operandArray);
-                    break;
                 case "+":
                     add(operandArray);
                     break;
@@ -93,32 +139,78 @@ let equalResult = function equalResult() {
                     div(operandArray);
                     break;
                 case "*":
-                    mult(operandArray);
+                mult(operandArray);
+                    break; 
+                case "-":
+                    sub(operandArray);
                     break;
-        };
-    };
+        }
+    } 
 };
 
 // Clear-Button function
-let clearCalc = function clearCalc() {
+let clearFunction = function clearFunction() {
     resultWindow.innerText = "";
     ansWindow.innerText = "";
     result = "";
     operator = "";
 }
 
+let delFunction = function delFunction() {
+    let container = resultWindow.innerText.slice(0, -1);
+    return resultWindow.innerText = container;;
+}
+
+let decimalFunction = function decimalFunction() {
+    let container = resultWindow.innerText.split("");
+    let multipleDecimals = function mutlipleDecimals() {
+        if (container.indexOf(".") !== -1) {
+            if (container.slice(container.indexOf("+")).indexOf(".") !== -1 ||
+            container.slice(container.indexOf("-")).indexOf(".") !== -1 ||
+            container.slice(container.indexOf("*")).indexOf(".") !== -1 ||
+            container.slice(container.indexOf("/")).indexOf(".") !== -1) {
+                return true;
+            } else {
+                return false;
+            }
+
+// hier geht es weiter: Was noch nicht funktioniert:
+// multiple Punkte sind möglich vor dem Operator (entweder hier oder weiter unten der Fehler)
+// außerdem: -Zahl steht im Komflikt mit oberem if Statement
+        } else if (container.indexOf("+") === -1 && container.indexOf(".") !== -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (resultWindow.innerText === "") {
+        return;
+    } else if (resultWindow.innerText.slice(-1) === "." || resultWindow.innerText.slice(-1) === "-" ||
+    resultWindow.innerText.slice(-1) === "+" || resultWindow.innerText.slice(-1) === "*" ||
+    resultWindow.innerText.slice(-1) === "/") {
+        return;
+    } else if (container.some(multipleDecimals) === true) {
+        return;
+    } else {
+        return resultWindow.innerText += ".";
+    }
+}
+
 // rules for the different button-click eventlisteners based on classes
 let input = function input(e) {
     if (e.target.className === "number") {
-        numberTransfer(e.target.innerText);
+        numberFunction(e.target.innerText);
     } else if (e.target.className === "operator") {
-        operatorTransfer(e.target.innerText);
+        operatorFunction(e.target.innerText);
     } else if (e.target.className === "equals") {
         equalResult();
     } else if (e.target.className === "clear") {
-        clearCalc();
-    } else {
-        return alert("Bonjour!");
+        clearFunction();
+    } else if (e.target.className === "del") {
+        delFunction();
+    } else if (e.target.className === "decimal") {
+        decimalFunction();
     };
 };
 
